02f749f0dc45dc5a573c7b6da550937e9e0cdb97
Kenolla <Kenolla@icloud.com>
2026-08-18 00:12:15 -0400

    Uploaded daz96050's Locks2 Fork for source

---
+++ Locks2/Source/Locks2/Core/Extensions.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Verse;
+
+namespace Locks2.Core
+{
+    public static class Extensions
+    {
+        private static readonly ConcurrentDictionary<int, int> _pawnSignature = new ConcurrentDictionary<int, int>();
+        private static readonly ConcurrentDictionary<int, LockComp> _cache = new ConcurrentDictionary<int, LockComp>();
+
+        public static LockConfig GetConfig(this Building door)
+        {
+            if (_cache.TryGetValue(door.thingIDNumber, out var comp)) return comp.config;
+            comp = door.GetComp<LockComp>();
+            if (comp == null)
+            {
+                if (Finder.debug)
+                {
+                    var message = string.Format("LOCKS2: Partially patched door {0}, {1}", door.def.defName, door);
+                    Log.Warning(message);
+                }
+
+                return null;
+            }
+
+            if (comp.config == null)
+            {
+                comp.config = new LockConfig { door = door };
+                comp.config.Initailize();
+            }
+
+            _cache[door.thingIDNumber] = comp;
+            return comp.config;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int GetKey(this Pawn pawn)
+        {
+            int hash;
+            unchecked
+            {
+                hash = pawn.GetSignature();
+                hash = pawn.thingIDNumber.GetHashCode() ^ (hash << 1);
+            }
+            return hash;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int GetSignature(this Pawn pawn, bool dirty = false)
+        {
+            int signature;
+            if (dirty || !_pawnSignature.TryGetValue(pawn.thingIDNumber, out signature))
+            {
+                signature = Rand.Int;
+                _pawnSignature[pawn.thingIDNumber] = signature;
+            }
+            return signature;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Notify_Dirty(this Pawn pawn)
+        {
+            var _ = pawn.GetSignature(true);
+        }
+    }
+}
---
+++ Locks2/Source/Locks2/Core/LockConfig.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace Locks2.Core
+{
+    public partial class LockConfig : IExposable
+    {
+        private static readonly List<LockConfig> configs = new List<LockConfig>();
+        private static readonly object configsLock = new object();
+
+        private readonly ConcurrentDictionary<int, Pair<bool, int>> cache = new ConcurrentDictionary<int, Pair<bool, int>>();
+        public Thing door;
+        public List<IConfigRule> rules;
+
+        public LockConfig()
+        {
+            lock (configsLock)
+            {
+                configs.Add(this);
+            }
+        }
+
+        public void ExposeData()
+        {
+            Scribe_References.Look(ref door, "door", false);
+            Scribe_Collections.Look(ref rules, "rules", LookMode.Deep);
+            if (Scribe.mode != LoadSaveMode.Saving && (rules == null || rules.Count == 0)) Initailize();
+        }
+
+        public static void Notify_Dirty()
+        {
+            lock (configsLock)
+            {
+                foreach (var config in configs)
+                {
+                    config.cache.Clear();
+                }
+            }
+            if (Find.CurrentMap == null) return;
+
+            foreach (var pawn in Find.CurrentMap.mapPawns.AllPawns)
+            {
+                pawn.Notify_Dirty();
+            }
+            Find.CurrentMap.reachability.ClearCache();
+        }
+
+        public bool Allows(Pawn pawn)
+        {
+            var key = pawn.GetKey();
+            if (cache.TryGetValue(key, out var store) &&
+                GenTicks.TicksGame - store.second < Finder.settings.cacheTimeOut) return store.first;
+            var result = AllowsInternal(pawn);
+            cache[key] = new Pair<bool, int>(result, GenTicks.TicksGame);
+            return result;
+        }
+
+        public void Dirty()
+        {
+            cache.Clear();
+        }
+
+        public void Dirty(Pawn pawn)
+        {
+            cache.TryRemove(pawn.thingIDNumber, out _);
+        }
+
+        private bool AllowsInternal(Pawn pawn)
+        {
+            foreach (var rule in rules)
+                if (rule.Allows(pawn))
+                    return true;
+            return false;
+        }
+
+        public void Initailize()
+        {
+            rules = new List<IConfigRule>();
+            if (Finder.settings.defaultRules.Count > 0)
+            {
+                foreach (var type in Finder.settings.defaultRules)
+                    rules.Add(Activator.CreateInstance(type) as IConfigRule);
+            }
+            else
+            {
+                var isFenceGate = (door?.def?.building?.isFence ?? false) 
+                    || (door?.def?.defName?.IndexOf("Gate", StringComparison.OrdinalIgnoreCase) >= 0 
+                        && door is Building_Door);
+                rules.Add(new ConfigRuleAnimals { enabled = !isFenceGate });
+                rules.Add(new ConfigRuleColonists());
+                rules.Add(new ConfigRuleMechanoids());
+                if (ModsConfig.IsActive("Neronix17.Asimov"))
+                    rules.Add(new ConfigRuleAutomatons());
+                rules.Add(new ConfigRuleGuests());
+                rules.Add(new ConfigRuleIgnorDrafted());
+            }
+        }
+
+        public abstract class IConfigRule : IExposable
+        {
+            public abstract float Height { get; }
+            public abstract void ExposeData();
+            public abstract bool Allows(Pawn pawn);
+
+            public abstract void DoContent(IEnumerable<Pawn> pawns, Rect rect, Action notifySelectionBegan,
+                Action notifySelectionEnded);
+
+            public abstract IConfigRule Duplicate();
+        }
+    }
+}
